398f2c3bca72e800275ce602c9e61142e6a19b33
Andrew Macgregor <andrew.macgregor@agworld.com.au>
2017-09-15 11:08:19 +0800

    Revert "Allow passing of cucumber command as ENV var"
    This reverts commit 30a426488d21e8741669359016a08d80657ffcf3.

--- lib/test_boosters/boosters/cucumber.rb
+++ lib/test_boosters/boosters/cucumber.rb
@@ -5,7 +5,7 @@ module TestBoosters
       FILE_PATTERN = "features/**/*.feature".freeze
 
       def initialize
-        super(FILE_PATTERN, split_configuration_path, command)
+        super(FILE_PATTERN, split_configuration_path, "bundle exec cucumber")
       end
 
       def before_job
@@ -30,10 +30,6 @@ module TestBoosters
 
       def split_configuration_path
         ENV["CUCUMBER_SPLIT_CONFIGURATION_PATH"] || "#{ENV["HOME"]}/cucumber_split_configuration.json"
-      end
-
-      def command
-        ENV['CUCUMBER_COMMAND'] || 'bundle exec cucumber'
       end
 
     end
