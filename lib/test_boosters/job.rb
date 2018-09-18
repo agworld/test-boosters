be7d78cd6f7c594c4315bdcf8a7e8d10a632c176
Aiden Sobey <aiden.sobey@agworld.com.au>
2018-09-18 08:23:49 +0800

    Hotfix for commands not executing.
    Left a bug in that caused jobs to not actually execute tasks.

--- lib/test_boosters/job.rb
+++ lib/test_boosters/job.rb
@@ -33,14 +33,7 @@ module TestBoosters
         return 0
       end
 
-      # TODO: do this properly, hack this in for now to get it working
-      if @command =~ /cucumber/
-        cmd = "if [ -x ./script/semaphore/cukes_off.rb ] && ./script/semaphore/cukes_off.rb; then echo 'CUKES OFF'; else bundle exec cucumber --strict -f rerun --out rerun.txt #{files.join(" ")} || bundle exec cucumber --strict @rerun.txt; fi"
-        TestBoosters::Shell.execute(cmd)
-      else
-        TestBoosters::Shell.execute("#{@command} #{files.join(" ")}")
-      end
+      TestBoosters::Shell.execute("#{@command} #{files.join(" ")}")
     end
-
   end
 end
